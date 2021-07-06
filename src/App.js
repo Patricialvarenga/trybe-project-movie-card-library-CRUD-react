9f4b4fb95ae333d4715ecd5c0ae4eb4cb2bb9aae
Patricia Silvestre Alvarenga <patriciasilvestrea@gmail.com>
2021-07-06 17:56:27 -0300

    requisito 1 e alteraçoes para passar no lint

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
-function App() {
-  return (
-    <div>Movie Card Library CRUD</div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <h2>Movie Card Library CRUD</h2>
+        <Route exact path="/" component={ MovieList } />
+        <Route exact path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/movies/new" component={ NewMovie } />
+        <Route exact path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="" component={ NotFound } />
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
